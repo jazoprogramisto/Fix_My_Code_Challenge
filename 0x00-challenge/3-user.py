11ad44eeb234e2ae29f5d364b7ecaa0da7d80771
i18nfoo <bygeoffreyouma@gmail.com>
2023-07-26 01:20:24 +0300

    User pswd

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,6 +54,8 @@ class User():
             return False
         if self.__password is None:
             return False
+        else:
+            pass
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
@@ -83,6 +85,10 @@ if __name__ == '__main__':
     user_2.password = 89
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
+
+    if not user_1.is_valid_password(u_pwd):
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
