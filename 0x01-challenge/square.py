d49fc90dda064706d3f73a3f1c859fe0d3665c83
alxdojo <bygeoffreyouma@gmail.com>
2023-09-06 15:32:53 -0700

    Named class User conventionally and added consistency in snake-case naming of variables

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
+"""
+Module for class Square
+"""
+
 
 class Square():
-    """Module for class Square
+    """ Defines square
+
+    Attributes:
+        - width: width of the square
+        - height: height of the square
     """
-    def __init__(self, width=0, height=0):
-        """Method that defines a square
-
-        Attributes:
-          - width: width of the square
-          - height: height of the square
-        """
-        self.width = width
-        self.height = heigiht
-
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Area of the square"""
+        """ Area of the square """
         return self.width * self.height
 
-    def PerimeterOfMySquare(self):
-        """Perimeter of the square"""
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
 
-if __name__ == "__Main__":
+if __name__ == "__main__":
+
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PerimeterOfMySquare())
+    print(s.perimeter_of_my_square())
